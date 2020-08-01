fa3dd063dcc6d10652f7ec969d033fe2ff0fd6b4
cloxnu <o18089541818@outlook.com>
2020-08-01 09:47:51 +0800

    Update home

--- js/darkmode.js
+++ js/darkmode.js
@@ -4,11 +4,17 @@ let dark_mode_img_name = "res/home/sun.png";
 
 change_dark_mode(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+window.matchMedia('(prefers-color-scheme: dark)').addListener(e => {
     const isDarkMode = e.matches;
     change_dark_mode(isDarkMode);
-    application_to_html(isDarkMode);
+    application_to_html();
 });
+
+// addEventListener('change', e => {
+//     const isDarkMode = e.matches;
+//     change_dark_mode(isDarkMode);
+//     application_to_html();
+// }
 
 function change_dark_mode(isDarkMode) {
     is_dark_mode = isDarkMode;
@@ -25,11 +31,20 @@ function change_dark_mode(isDarkMode) {
 function application_to_html() {
     document.body.classList.remove(is_dark_mode ? "dark-mode-off" : "dark-mode-on");
     document.body.classList.add(is_dark_mode ? "dark-mode-on" : "dark-mode-off");
-    document.getElementById("dark-mode-img").src = dark_mode_img_name;
     document.getElementById("dark-mode-input").checked = is_dark_mode;
+    dark_mode_img_display();
+    dark_mode_label_flip();
+
+}
+
+function dark_mode_label_flip() {
     if (is_dark_mode)
         document.getElementById("dark-mode-label").classList.remove("flip");
     else
         document.getElementById("dark-mode-label").classList.add("flip");
+}
 
+function dark_mode_img_display() {
+    document.getElementById("dark-mode-sun-img").style.display = is_dark_mode ? "none" : "block";
+    document.getElementById("dark-mode-moon-img").style.display = is_dark_mode ? "block" : "none";
 }
