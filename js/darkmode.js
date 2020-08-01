b008c483852cd0d074cadb746cdcdc03598651b3
cloxnu <o18089541818@outlook.com>
2020-08-01 19:11:15 +0800

    Update home

--- js/darkmode.js
+++ js/darkmode.js
@@ -25,7 +25,6 @@ function application_to_html() {
     document.getElementById("dark-mode-input").checked = is_dark_mode;
     dark_mode_img_display();
     dark_mode_label_flip();
-
 }
 
 function dark_mode_label_flip() {
--- js/home.js
+++ js/home.js
@@ -7,4 +7,12 @@ readall(function (json) {
 function dark_mode_clicked() {
     change_dark_mode(!is_dark_mode);
     application_to_html();
+
+    head_logo_display();
+}
+
+function head_logo_display() {
+    document.getElementById("head-shelter").style.opacity = is_dark_mode ? "0.3" : "0";
+    document.getElementById("head-logo-black").style.opacity = is_dark_mode ? "0" : "1";
+    document.getElementById("head-logo-white").style.opacity = is_dark_mode ? "1" : "0";
 }
